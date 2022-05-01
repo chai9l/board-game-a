e579bb90e24f38252dee4ad5c557b7d88af1c7c9
chai9l <62384385+chai9l@users.noreply.github.com>
2022-05-01 17:43:42 +0300

    Add files via upload

---
+++ Ambassador.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+#include <vector>
+#include "Ambassador.hpp"
+
+namespace coup
+{
+    Ambassador::Ambassador(Game &game, const std::string &name) {}
+
+    void Ambassador::transfer(Player &p1, Player &p2) {}
+
+    void Ambassador::block(Player &p) {}
+
+    Ambassador::~Ambassador() {}
+}
---
+++ Player.cpp
@@ -0,0 +1,25 @@
+#include "iostream"
+#include <vector>
+#include "Player.hpp"
+
+namespace coup
+{
+
+    void Player::income()
+    {
+    }
+    void Player::foreign_aid()
+    {
+    }
+    void Player::coup(Player &p)
+    {
+    }
+    int Player::coins()
+    {
+        return 1;
+    }
+    std::string Player::role()
+    {
+        return " ";
+    }
+}
